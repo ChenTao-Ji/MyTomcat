deb97a1e675ccf95e842ebc1dd168d6605a9f28c
chentao <chentao.ji@yooli.com>
2019-04-26 17:21:36 +0800

    Initial commit

---
+++ src/com/tomcat/servlet/LoginServlet.java
@@ -0,0 +1,27 @@
+package com.tomcat.servlet;
+
+import com.tomcat.http.Request;
+import com.tomcat.http.Response;
+
+import java.io.OutputStream;
+
+public class LoginServlet extends Servlet {
+
+    @Override
+    public void doGet(Request request, Response response) {
+        this.doPost(request, response);
+    }
+
+    @Override
+    public void doPost(Request request, Response response) {
+        try {
+            OutputStream outputStream = response.outputStream;
+            String res = Response.responseHeader + "Hello,welcome to here !";
+            outputStream.write(res.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/com/tomcat/servlet/Servlet.java
@@ -0,0 +1,23 @@
+package com.tomcat.servlet;
+
+import com.tomcat.http.Request;
+import com.tomcat.http.Response;
+
+public abstract class Servlet {
+
+    public void service(Request request, Response response) {
+
+        //判断是调用doget 还是 dopost
+        if ("get".equalsIgnoreCase(request.getMethod())) {
+            this.doGet(request, response);
+        } else {
+            this.doPost(request, response);
+        }
+
+    }
+
+    public abstract void doGet(Request request, Response response);
+
+    public abstract void doPost(Request request, Response response);
+
+}
